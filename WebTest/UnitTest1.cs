f7bc6b5125a29a03b98604997ede33d9c6df1697
MAJU STUDENT <majustudent@jinnah.edu>
2019-05-25 17:27:54 +0500

    second

--- WebTest/UnitTest1.cs
+++ WebTest/UnitTest1.cs
@@ -31,22 +31,34 @@ namespace WebTest
             var drpdwnObj = new SelectElement(drpdwn);
             drpdwnObj.SelectByText("Admin");
             page.FindElement(By.Id("searchBtn")).Click();
+            page.FindElement(By.Id("resetBtn")).Click();
             page.FindElement(By.Id("btnAdd")).Click();
             page.FindElement(By.Id("systemUser_userType")).SendKeys("Admin");
             page.FindElement(By.Id("systemUser_employeeName_empName")).SendKeys("Tester");
-             
-             //Assert.AreEqual("")
-             
+            page.FindElement(By.Id("btnCancel")).Click();
+            page.FindElement(By.Id("menu_admin_Job")).Click();
+            page.FindElement(By.Id("menu_admin_viewJobTitleList")).Click();
+            page.FindElement(By.Id("btnAdd")).Click();
+            page.FindElement(By.Id("jobTitle_jobTitle")).SendKeys("Assistant Manager");
+            page.FindElement(By.Id("jobTitle_jobDescription")).SendKeys("Managerial Position");
+            page.FindElement(By.Id("jobTitle_note")).SendKeys("Best of luck!!!");
+            page.FindElement(By.Id("btnSave")).Click();
+            //Assert.AreEqual("")
+
         }
         [TestMethod]
         public void Logout()
         {
-            //IWebDriver page = new ChromeDriver();
-            //page.Url = "https://opensource-demo.orangehrmlive.com/";
+            IWebDriver page = new ChromeDriver();
+            page.Url = "https://opensource-demo.orangehrmlive.com/";
+            page.Manage().Window.Maximize();
+            page.FindElement(By.Id("txtUsername")).SendKeys("Admin");
+            page.FindElement(By.Id("txtPassword")).SendKeys("admin123");
+            page.FindElement(By.Id("btnLogin")).Click();
             //page.FindElement(By.Id("Mp_btn")).Click();
             //Assert.AreEqual("")
-            //page.FindElement(By.Id("welcome-menu")).Click();
-
+            page.FindElement(By.Id("welcome")).Click();
+            page.FindElement(By.XPath("//*[@Logout]")).Click();
         }
     }
 }
